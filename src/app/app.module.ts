2f65013dafa0780e9d72f94561b54f93dd81a63f
Francisco Fontes <fontes.fj@gmail.com>
2021-03-29 17:41:15 -0300

    Criacao do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule
